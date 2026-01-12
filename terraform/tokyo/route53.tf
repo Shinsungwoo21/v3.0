64d468a2957b9efdcfc70a87d5cfca0191ca2db2
playdelaybluelay-stack <playdelaybluelay@gmail.com>
2026-01-12 17:15:49 +0900

    fix actinos error

--- terraform/tokyo/route53.tf
+++ terraform/tokyo/route53.tf
@@ -5,21 +5,21 @@
 # -----------------------------------------------------------------------------
 # Secondary Failover Record (No Health Check)
 # -----------------------------------------------------------------------------
-'''resource "aws_route53_record" "api_secondary" {
-  zone_id = var.route53_zone_id
-  name    = "api.megaticket.click"
-  type    = "A"
-
-  failover_routing_policy {
-    type = "SECONDARY"
-  }
-
-  set_identifier = "plcr-api-secondary-${var.region_code}"
-
-  alias {
-    name                   = aws_lb.dr.dns_name
-    zone_id                = aws_lb.dr.zone_id
-    evaluate_target_health = true
-  }
-}'''
+# resource "aws_route53_record" "api_secondary" {
+#  zone_id = var.route53_zone_id
+#  name    = "api.megaticket.click"
+#  type    = "A"
+#
+#  failover_routing_policy {
+#    type = "SECONDARY"
+#  }
+#
+#  set_identifier = "plcr-api-secondary-${var.region_code}"
+#
+#  alias {
+#    name                   = aws_lb.dr.dns_name
+#    zone_id                = aws_lb.dr.zone_id
+#    evaluate_target_health = true
+#  }
+#}
 
