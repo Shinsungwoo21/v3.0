830322ad0d2cbea25e173d3215e3129472c66933
Yebin Han <playdelaybluelay@gmail.com>
2026-01-26 12:12:29 +0900

    Add frontend environment variables for Cognito

--- apps/web/config.js
+++ apps/web/config.js
@@ -1,5 +1,8 @@
 ﻿window.__PLCR_CONFIG__ = {
   API_URL: "https://api.megaticket.click",
+  COGNITO_USER_POOL_ID: "ap-northeast-2_CeuKMd4UK",
+  COGNITO_CLIENT_ID: "c93ngcv02luutncs2r6ogt5nh",
+  COGNITO_DOMAIN: "auth.megaticket.click",
   AWS_REGION: "ap-northeast-2",
   PROJECT: "plcr",
   ENVIRONMENT: "prod"
