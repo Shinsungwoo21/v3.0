9cc5afb60623b3ecd73d4d11acc54365c24bf734
Yebin Han <playdelaybluelay@gmail.com>
2026-01-26 15:52:11 +0900

    fix cognito client id

--- apps/web/config.js
+++ apps/web/config.js
@@ -1,7 +1,7 @@
 ﻿window.__PLCR_CONFIG__ = {
   API_URL: "https://api.megaticket.click",
   COGNITO_USER_POOL_ID: "ap-northeast-2_CeuKMd4UK",
-  COGNITO_CLIENT_ID: "1vjgbu2a0k3tha9i808h0fu5ie",
+  COGNITO_CLIENT_ID: "7eq07al29432spinq2elds01gi",
   COGNITO_DOMAIN: "auth.megaticket.click",
   AWS_REGION: "ap-northeast-2",
   PROJECT: "plcr",
--- apps/web/public/config.js
+++ apps/web/public/config.js
@@ -6,7 +6,7 @@ window.__PLCR_CONFIG__ = {
   PROJECT: "plcr",
   ENVIRONMENT: "prod",
   COGNITO_USER_POOL_ID: "ap-northeast-2_CeuKMd4UK",
-  COGNITO_CLIENT_ID: "1vjgbu2a0k3tha9i808h0fu5ie",
+  COGNITO_CLIENT_ID: "7eq07al29432spinq2elds01gi",
   COGNITO_DOMAIN: "auth.megaticket.click",
   AUTH_ENABLED: true,
   AUTH_PROVIDER: "cognito"
