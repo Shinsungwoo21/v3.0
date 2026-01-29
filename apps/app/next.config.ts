da79f01a7becddbaabb05e94def9884d56b01870
Shinsungwoo21 <tlstjd1007@naver.com>
2026-01-29 12:35:08 +0900

    fix: buildspec.yml 수정 및 next.config.ts 코드 추가

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output: 'standalone',
     transpilePackages: ['@mega-ticket/shared-types', '@mega-ticket/shared-utils'],
     eslint: {
         ignoreDuringBuilds: true,
